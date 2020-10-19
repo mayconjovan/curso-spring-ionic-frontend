a11f3b260a7d6a35c981549b5d8d903102a3ff0f
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-19 15:48:06 -0300

    Interceptor to include token in requests

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -12,17 +12,12 @@ export class ClienteService {
     }
 
     findByEmail(email: string): Observable<ClienteDTO> {
-        let token = this.storage.getLocaluser().token;
-        let authHeader = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clients/email?value=${email}`,
-            { 'headers': authHeader });
-
+        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clients/email?value=${email}`)
     }
 
-    getImageFromBucket(id: string) : Observable<any> {
+    getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType: 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
     }
 
 }
