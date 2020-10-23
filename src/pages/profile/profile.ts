61b4cee9e7056c48aa19544f3c33f6f3f5a7271e
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-22 22:27:45 -0300

    Doing picutre upload

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -27,6 +27,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocaluser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -69,5 +73,20 @@ export class ProfilePage {
 
     });
 
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    },
+    error => {
+
+    });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -3,11 +3,15 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { API_CONFIG } from "../../config/api.config";
 import { ClienteDTO } from "../../models/cliente.dto";
+import { ImageUtilService } from "../image-util.service";
 import { StorageService } from "../storage.services";
 
 @Injectable()
 export class ClienteService {
-    constructor(public http: HttpClient, public storage: StorageService) {
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService) {
 
     }
 
@@ -28,6 +32,19 @@ export class ClienteService {
         return this.http.post(
             `${API_CONFIG.baseUrl}/clients`,
             obj,{
+                observe: 'response',
+                responseType: 'text'
+            }
+        )
+    }
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.append('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clients/picture`,
+            formData,{
                 observe: 'response',
                 responseType: 'text'
             }
